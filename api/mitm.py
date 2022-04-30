389d088c4b2e4c2a722622493ddb5757543f132a
zhujq <=>
2022-05-01 00:52:42 +0800

    v0.1

--- api/mitm.py
+++ api/mitm.py
@@ -30,6 +30,8 @@ class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
+        self.end_headers()
+        self.wfile.write(self.headers.encode())
         print(self.headers)
         print(self.command)
         return
