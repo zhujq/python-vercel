5e9c2a00901794a7f5547096226d7a58b974c787
zhujq <=>
2022-05-01 00:07:39 +0800

    v0.1

--- api/mitm.py
+++ api/mitm.py
@@ -4,31 +4,33 @@ import pickle
 
 class handler(BaseHTTPRequestHandler):
 
-  def do_POST(self):
+    def do_POST(self):
    
-    data = self.rfile.read(int(self.headers['content-length']))
-    kwargs = json.loads(data)
-    kwargs['data'] = base64.b64decode(kwargs['data'])
-    print(kwargs['data']["url"])
+        data = self.rfile.read(int(self.headers['content-length']))
+        kwargs = json.loads(data)
+        kwargs['data'] = base64.b64decode(kwargs['data'])
+        print(kwargs['data']["url"])
 
-    try:
-        req = requests.request(**kwargs, verify=False, allow_redirects=False)
-        serializedReq = pickle.dumps(req)
+        try:
+            req = requests.request(**kwargs, verify=False, allow_redirects=False)
+            serializedReq = pickle.dumps(req)
 
-        self.send_response(200)
-        self.end_headers()
-    #   self.wfile.write(base64.b64encode(serializedReq).decode("utf-8"))
-        self.wfile.write(base64.b64encode(serializedReq))
+            self.send_response(200)
+            self.end_headers()
+         #  self.wfile.write(base64.b64encode(serializedReq).decode("utf-8"))
+            self.wfile.write(base64.b64encode(serializedReq))
         
-    except Exception as e:#可以以集群方式返回结果抛出异常
-        exc_type, exc_value, exc_traceback = sys.exc_info()
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(str(exc_value).encode().decode("utf-8"))
+        except Exception as e:#可以以集群方式返回结果抛出异常
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(str(exc_value).encode().decode("utf-8"))
         
-    return
+        return
 
-def do_GET(self):
-    print(self.headers)
-    print(self.command)
-    return
+    def do_GET(self):
+        self.send_response(200)
+        print(self.headers)
+        print(self.command)
+        self.wfile.write(self.headers)
+        return
